18d6193d56a06b3e245b454a3b9d6d853bd14c7e
JangGwangJin <alfus9802@naver.com>
2024-08-30 18:36:07 +0900

    @gjjang
    사전 세팅

---
+++ matServer/MatTamjeong/src/main/java/com/example/demo/MatTamjeongApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatTamjeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatTamjeongApplication.class, args);
+	}
+
+}
