30844356182b52fbd7ec9054db98678f745fe9e1
JangGwangJin <alfus9802@naver.com>
2024-09-02 01:19:51 +0900

    @gjjang

--- matServer/Crawling/src/test/java/com/crawler/CrawlingApplicationTests.java
+++ matServer/Crawling/src/test/java/com/crawler/CrawlingApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.crawler;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MatTamjeongApplicationTests {
+class CrawlingApplicationTests {
 
 	@Test
 	void contextLoads() {
