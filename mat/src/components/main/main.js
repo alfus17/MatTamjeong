5d8cf8f6fe1d6db9067e82894ab74b09dfc657d2
JangGwangJin <alfus9802@naver.com>
2024-09-07 01:18:08 +0900

    @gja;lsd
    asd

--- mat/src/components/main/main.js
+++ mat/src/components/main/main.js
@@ -5,6 +5,16 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../css/main.css';
 
+
+
+
+
+
+
+
+
+
+
 function Main() {
   const [store, setStore] = useState([]);
   const [filterStore, setFilterStore] = useState([]);
