19e9216bcbb6a8ef325e554848d1532417e43a15
JangGwangJin <alfus9802@naver.com>
2024-09-10 01:46:43 +0900

    @gjjang 머지파일 및 검색기능 백엔드 작업 진행중
    @gjjang 머지파일 및 검색기능 백엔드 작업 진행중

--- matServer/MatTamjeong/src/main/java/com/mat/controller/SearchPageController.java
+++ matServer/MatTamjeong/src/main/java/com/mat/controller/SearchPageController.java
@@ -1,10 +1,43 @@
 package com.mat.controller;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mat.domain.Store;
+import com.mat.service.StoreService;
 
 @RestController
 @RequestMapping("/Search")
 public class SearchPageController {
+	
+	// 가게 
+	@Autowired
+	private StoreService StoreService;
+	
+	
+	// 
+	@PostMapping("/")
+	public List<Object> findStoresWithKeywords (@RequestBody String keyword){
+		
+		Set<Store> stores = new HashSet<>();
+		
+		stores.add();
+		
+			
+		return null;
+	}
+	
+	
+	
+	
 
 }
