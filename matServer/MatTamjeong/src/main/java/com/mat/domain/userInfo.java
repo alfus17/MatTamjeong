8c43de1e6d0e5ae43577f468c9850155f7973072
asd0916 <theyangs98@naver.com>
2024-09-11 16:45:59 +0900

    1645

--- matServer/MatTamjeong/src/main/java/com/mat/domain/userInfo.java
+++ matServer/MatTamjeong/src/main/java/com/mat/domain/userInfo.java
@@ -1,5 +1,9 @@
 package com.mat.domain;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
 import io.micrometer.common.lang.NonNull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,48 +18,81 @@ import lombok.NoArgsConstructor;
 @Entity(name="user_Info")
 @Table(name="user_Info")
 public class userInfo {
-    /* 	user_info Table
- * 	회원 ID (PK)(FK) 	: user_id			varchar2(20)
- *  주소 				: user_address		varchar2(100)
- * 	상태				: user_status		varchar2(5)
- * 	리뷰ID (FK)		: review_id			varchar2(10)
- * 	즐겨찾기(FK)		: bookmark_id		varchar2(10)
- * 	auth(사용여부미정)	: auth				varchar2(1000)
+    /*    user_info Table
+ *    회원 ID (PK)(FK)    : user_id         varchar2(20)
+ * 
+ *    이름             : user_name       varchar2(60)
+ *    가입일자          : member_join_date   Date
+ *  닉네임           : nickname         varchar2(20)
+ *  생년월일          : user_birth      varchar2(6)
+ *  이메일          : user_email      varchar2(100)
+ *  
+ *  주소             : user_address      varchar2(100)
+ *    상태            : user_status      varchar2(5)
+ *    리뷰ID (FK)      : review_id         varchar2(10)
+ *    즐겨찾기(FK)      : bookmark_id      varchar2(10)
+ *    auth(사용여부미정)   : auth            varchar2(1000)
  */
-	// 회원 ID (PK)(FK)
-	// 백엔드 쪽에서 FK 처리예정
-	@Id
-	@Column(name="user_id" ,length=20)
-	@NonNull
-	@GeneratedValue
-	private String userId;
-	
-	//  주소
-	@Column(name="user_address" ,length=100)
-	@NonNull
-	private String userAddress;
+   // 회원 ID (PK)(FK)
+   // 백엔드 쪽에서 FK 처리예정
+   @Id
+   @Column(name="user_id" ,length=20)
+   @NonNull
+   @GeneratedValue
+   private String userId;
+   
+   //  이름
+   @Column(name="user_name" ,length=60)
+   @NonNull
+   private String userName;
+   
+   //  가입일자
+   @CreatedDate
+   @Column(name="create_at" )
+   @NonNull
+   private LocalDateTime createAt;
+   
+   //  닉네임
+   @Column(name="nickname" ,length=20)
+   @NonNull
+   private String nickName;
+   
+   //  생년월일
+   @Column(name="user_birth" ,length=6)
+   @NonNull
+   private String userBirth;
+   
+   //  이메일
+   @Column(name="email" ,length=100)
+   @NonNull
+   private String email;
+   
+   //  주소
+   @Column(name="user_address" ,length=100)
+   @NonNull
+   private String userAddress;
 
-	// 상태	
-	@Column(name="user_status" ,length=5)
-	@NonNull
-	private String userStatus;
-	
-	// 리뷰ID (FK)
-	@Column(name="review_id" ,length=10)
-	@NonNull
-	private String reviewId;
-	
-	// 즐겨찾기(FK)
-	@Column(name="bookmark_id" ,length=10)
-	@NonNull
-	private String bookmarkId;
-	
-	// auth( 사용여부 미정
-	@Column(name="auth" ,length=1000)
-	@NonNull
-	private String auth;
+   // 상태   
+   @Column(name="user_status" ,length=5)
+   @NonNull
+   private String userStatus;
+   
+   // 리뷰ID (FK)
+   @Column(name="review_id" ,length=10)
+   @NonNull
+   private String reviewId;
+   
+   // 즐겨찾기(FK)
+   @Column(name="bookmark_id" ,length=10)
+   @NonNull
+   private String bookmarkId;
+   
+   // auth( 사용여부 미정
+   @Column(name="auth" ,length=1000)
+   @NonNull
+   private String auth;
 
 
-	
+   
 
 }
